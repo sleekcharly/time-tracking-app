57c0344201f813fe9c0511682027b52f19f18e5d
sleekcharly <c_ukasoanya@yahoo.com>
2019-12-04 07:13:16 +0100

    Added state to ToggleableTimerForm component

--- public/js/app.js
+++ public/js/app.js
@@ -116,15 +116,27 @@ class TimerForm extends React.Component {
 }
 
 class ToggleableTimerForm extends React.Component {
+    state = {
+        isOpen: false,
+    };
+
+    handleFormOpen = () => {
+        this.setState({
+            isOpen: true
+        });
+    };
+
     render() {
-        if (this.props.isOpen) {
+        if (this.state.isOpen) {
             return (
                 <TimerForm />
             );
         } else {
             return (
                 <div className='ui basic content center aligned segment'>
-                    <button className='ui basic button icon'>
+                    <button className='ui basic button icon'
+                            onClick={this.handleFormOpen}
+                    >
                         <i className='plus icon' />
                     </button>
                 </div>
