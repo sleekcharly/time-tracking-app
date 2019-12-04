9cd655b969be6c2692a5024f961c1a37a9136fa9
sleekcharly <c_ukasoanya@yahoo.com>
2019-12-04 08:06:21 +0100

    updated EditableTimer component.

--- public/js/app.js
+++ public/js/app.js
@@ -72,6 +72,27 @@ class EditableTimer extends React.Component {
         editFormOpen: false,
     };
 
+    handleEditClick = () => {
+        this.openForm();
+    };
+
+    handleFormClose = () => {
+        this.closeForm();
+    };
+
+    handleSubmit = (timer) => {
+        this.props.onFormSubmit(timer);
+        this.closeForm();
+    };
+
+    closeForm = () => {
+        this.setState({ editFormOpen: false });
+    };
+
+    openForm = () => {
+        this.setState({ editFormOpen: true });
+    };
+
     render() {
         if (this.state.editFormOpen) {
             return (
@@ -79,6 +100,8 @@ class EditableTimer extends React.Component {
                     id={this.props.id}
                     title={this.props.title}
                     project={this.props.project}
+                    onFormSubmit={this.handleSubmit}
+                    onFormClose={this.handleFormClose}
                 />
             );
         } else {
@@ -89,6 +112,7 @@ class EditableTimer extends React.Component {
                     project={this.props.project}
                     elapsed={this.props.elapsed}
                     runningSince={this.props.runningSince}
+                    onEditClick={this.handleEditClick}
                 />
             );
         }
