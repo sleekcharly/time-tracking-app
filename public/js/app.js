66e7b3951aecc43e5d10b1430f7fd503bccaf319
sleekcharly <c_ukasoanya@yahoo.com>
2019-12-04 08:07:48 +0100

    Updated EditableTimerList component

--- public/js/app.js
+++ public/js/app.js
@@ -57,8 +57,10 @@ class EditableTimerList extends React.Component {
                 project={timer.project}
                 elapsed={timer.elapsed}
                 runningSince={timer.runningSince}
+                onFormSubmit={this.props.onFormSubmit}
             />
         ));
+        
         return (
             <div id='timers'>
                 {timers}
