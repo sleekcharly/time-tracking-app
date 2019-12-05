ba9f1b39d6aedda4a8feccad76264627dcf32b11
sleekcharly <c_ukasoanya@yahoo.com>
2019-12-05 10:10:10 +0100

    Sending creates, updates, and deletes to the server

--- public/js/app.js
+++ public/js/app.js
@@ -25,6 +25,8 @@ class TimersDashboard extends React.Component {
         this.setState({
             timers: this.state.timers.concat(t),
         });
+
+        client.createTimer(t);
     };
 
     handleEditFormSubmit = (attrs) => {
@@ -56,12 +58,18 @@ class TimersDashboard extends React.Component {
                 }
             }),
         });
+
+        client.updateTimer(attrs);
     };
 
     deleteTimer = (timerId) => {
         this.setState({
             timers: this.state.timers.filter(t => t.id !== timerId),
         });
+
+        client.deleteTimer(
+            { id: timerId }
+        );
     };
 
     startTimer = (timerId) => {
@@ -78,6 +86,10 @@ class TimersDashboard extends React.Component {
                 }
             }),
         });
+
+        client.startTimer(
+            { id: timerId, start: now }
+        );
     };
 
     stopTimer = (timerId) => {
@@ -96,6 +108,10 @@ class TimersDashboard extends React.Component {
                 }
             }),
         });
+
+        client.stopTimer(
+            { id: timerId, stop: now }
+        );
     };
 
     render() {
